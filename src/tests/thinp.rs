a7e6c51ed2c2645539c8040d8d3a6b3eea06924e
Joe Thornber <ejt@redhat.com>
2021-03-19 10:55:22 +0000

    Generate csv file of thinp insert_block operation costs.

--- src/tests/thinp.rs
+++ src/tests/thinp.rs
@@ -15,8 +15,7 @@ use crate::wrappers::thinp_metadata::*;
 //-------------------------------
 
 #[allow(dead_code)]
-struct ThinPool<'a> {
-    fix: &'a mut Fixture,
+struct ThinPool {
     nr_metadata_blocks: u64,
     pmd: Addr,
     baseline: Stats,
@@ -26,9 +25,9 @@ struct ThinDev {
     td: Addr,
 }
 
-impl<'a> ThinPool<'a> {
+impl ThinPool {
     fn new(
-        fix: &'a mut Fixture,
+        fix: &mut Fixture,
         nr_metadata_blocks: u64,
         data_block_size: u64,
         nr_data_blocks: u64,
@@ -39,72 +38,71 @@ impl<'a> ThinPool<'a> {
         let baseline = Stats::collect_stats(fix);
 
         Ok(ThinPool {
-            fix,
             nr_metadata_blocks,
             pmd,
             baseline,
         })
     }
 
-    fn stats_start(&mut self) {
-        self.baseline = Stats::collect_stats(self.fix);
+    fn stats_start(&mut self, fix: &mut Fixture) {
+        self.baseline = Stats::collect_stats(fix);
     }
 
-    fn stats_report(&mut self, desc: &str, count: u64) -> Result<()> {
-        let delta = self.baseline.delta(self.fix);
+    fn stats_report(&mut self, fix: &mut Fixture, desc: &str, count: u64) -> Result<()> {
+        let delta = self.baseline.delta(fix);
         info!(
             "{}: instrs = {}, read_locks = {:.1}, write_locks = {:.1}, metadata = {}/{}",
             desc,
             delta.instrs / count,
             delta.read_locks as f64 / count as f64,
             delta.write_locks as f64 / count as f64,
-            self.nr_metadata_blocks - self.free_metadata_blocks()?,
+            self.nr_metadata_blocks - self.free_metadata_blocks(fix)?,
             self.nr_metadata_blocks,
         );
         Ok(())
     }
 
-    fn create_thin(&mut self, thin_id: ThinId) -> Result<()> {
-        dm_pool_create_thin(self.fix, self.pmd, thin_id)
+    fn create_thin(&mut self, fix: &mut Fixture, thin_id: ThinId) -> Result<()> {
+        dm_pool_create_thin(fix, self.pmd, thin_id)
     }
 
-    fn create_snap(&mut self, origin_id: ThinId, snap_id: ThinId) -> Result<()> {
-        dm_pool_create_snap(self.fix, self.pmd, origin_id, snap_id)
+    fn create_snap(&mut self, fix: &mut Fixture, origin_id: ThinId, snap_id: ThinId) -> Result<()> {
+        dm_pool_create_snap(fix, self.pmd, origin_id, snap_id)
     }
 
-    fn delete_thin(&mut self, thin_id: ThinId) -> Result<()> {
-        dm_pool_delete_thin_device(self.fix, self.pmd, thin_id)
+    fn delete_thin(&mut self, fix: &mut Fixture, thin_id: ThinId) -> Result<()> {
+        dm_pool_delete_thin_device(fix, self.pmd, thin_id)
     }
 
-    fn commit(&mut self) -> Result<()> {
-        dm_pool_commit_metadata(self.fix, self.pmd)
+    fn commit(&mut self, fix: &mut Fixture) -> Result<()> {
+        dm_pool_commit_metadata(fix, self.pmd)
     }
 
-    fn open_thin(&mut self, thin_id: ThinId) -> Result<ThinDev> {
-        let td = dm_pool_open_thin_device(self.fix, self.pmd, thin_id)?;
+    fn open_thin(&mut self, fix: &mut Fixture, thin_id: ThinId) -> Result<ThinDev> {
+        let td = dm_pool_open_thin_device(fix, self.pmd, thin_id)?;
         Ok(ThinDev { td })
     }
 
-    fn close_thin(&mut self, td: ThinDev) -> Result<()> {
-        dm_pool_close_thin_device(self.fix, td.td)
+    fn close_thin(&mut self, fix: &mut Fixture, td: ThinDev) -> Result<()> {
+        dm_pool_close_thin_device(fix, td.td)
     }
 
-    fn alloc_data_block(&mut self) -> Result<u64> {
-        dm_pool_alloc_data_block(self.fix, self.pmd)
+    fn alloc_data_block(&mut self, fix: &mut Fixture) -> Result<u64> {
+        dm_pool_alloc_data_block(fix, self.pmd)
     }
 
-    fn insert_block(&mut self, td: &ThinDev, thin_b: u64, data_b: u64) -> Result<()> {
-        dm_thin_insert_block(self.fix, td.td, thin_b, data_b)
+    fn insert_block(
+        &mut self,
+        fix: &mut Fixture,
+        td: &ThinDev,
+        thin_b: u64,
+        data_b: u64,
+    ) -> Result<()> {
+        dm_thin_insert_block(fix, td.td, thin_b, data_b)
     }
 
-    fn free_metadata_blocks(&mut self) -> Result<u64> {
-        dm_pool_get_free_metadata_block_count(self.fix, self.pmd)
-    }
-}
-
-impl<'a> Drop for ThinPool<'a> {
-    fn drop(&mut self) {
-        dm_pool_metadata_close(self.fix, self.pmd).expect("dm_pool_metadata_close() failed");
+    fn free_metadata_blocks(&mut self, fix: &mut Fixture) -> Result<u64> {
+        dm_pool_get_free_metadata_block_count(fix, self.pmd)
     }
 }
 
@@ -120,13 +118,13 @@ fn test_create_many_thins(fix: &mut Fixture) -> Result<()> {
     standard_globals(fix)?;
     let mut t = ThinPool::new(fix, 10240, 64, 102400)?;
     for tid in 0..1000 {
-        t.create_thin(tid)?;
+        t.create_thin(fix, tid)?;
     }
 
-    t.commit()?;
+    t.commit(fix)?;
 
     for tid in 0..1000 {
-        t.delete_thin(tid)?;
+        t.delete_thin(fix, tid)?;
     }
 
     Ok(())
@@ -137,21 +135,21 @@ fn test_create_rolling_snaps(fix: &mut Fixture) -> Result<()> {
 
     let count = 1000;
     let mut t = ThinPool::new(fix, 10240, 64, 102400)?;
-    t.create_thin(0)?;
+    t.create_thin(fix, 0)?;
 
     for snap in 1..count {
-        t.create_snap(snap, snap - 1)?;
+        t.create_snap(fix, snap, snap - 1)?;
     }
 
     // A commit is needed, otherwise delete will not work (not sure why,
     // or if this is a feature).
-    t.commit()?;
+    t.commit(fix)?;
 
     for tid in 0..count {
-        t.delete_thin(tid)?;
+        t.delete_thin(fix, tid)?;
     }
 
-    t.commit()?;
+    t.commit(fix)?;
 
     Ok(())
 }
@@ -162,28 +160,28 @@ fn do_provision_single_thin(fix: &mut Fixture, thin_blocks: &[u64]) -> Result<()
     standard_globals(fix)?;
 
     let mut pool = ThinPool::new(fix, 10240, 64, 102400)?;
-    pool.create_thin(0)?;
-    let td = pool.open_thin(0)?;
+    pool.create_thin(fix, 0)?;
+    let td = pool.open_thin(fix, 0)?;
 
     let mut insert_count = 0;
-    pool.stats_start();
+    pool.stats_start(fix);
     for thin_b in thin_blocks {
-        let data_b = pool.alloc_data_block()?;
-        pool.insert_block(&td, *thin_b, data_b)?;
+        let data_b = pool.alloc_data_block(fix)?;
+        pool.insert_block(fix, &td, *thin_b, data_b)?;
         insert_count += 1;
 
         if insert_count == commit_interval {
-            pool.commit()?;
-            pool.stats_report("provision", commit_interval)?;
-            pool.stats_start();
+            pool.commit(fix)?;
+            pool.stats_report(fix, "provision", commit_interval)?;
+            pool.stats_start(fix);
             insert_count = 0;
         }
     }
 
     // A commit is needed, otherwise delete will not work (not sure why,
     // or if this is a feature).
-    pool.commit()?;
-    pool.close_thin(td)?;
+    pool.commit(fix)?;
+    pool.close_thin(fix, td)?;
 
     Ok(())
 }
@@ -247,35 +245,35 @@ fn do_provision_recursive_snap(fix: &mut Fixture, thin_blocks: &[u64]) -> Result
 
     let mut pool = ThinPool::new(fix, 10240, 64, thin_blocks.len() as u64)?;
     let mut thin_id = 0;
-    pool.create_thin(0)?;
-    let mut td = pool.open_thin(0)?;
+    pool.create_thin(fix, 0)?;
+    let mut td = pool.open_thin(fix, 0)?;
 
     let mut insert_count = 0;
-    pool.stats_start();
+    pool.stats_start(fix);
     for thin_b in thin_blocks {
-        let data_b = pool.alloc_data_block()?;
-        pool.insert_block(&td, *thin_b, data_b)?;
+        let data_b = pool.alloc_data_block(fix)?;
+        pool.insert_block(fix, &td, *thin_b, data_b)?;
         insert_count += 1;
 
         if insert_count == commit_interval {
-            pool.commit()?;
-            pool.stats_report("provision", commit_interval)?;
+            pool.commit(fix)?;
+            pool.stats_report(fix, "provision", commit_interval)?;
 
-            pool.close_thin(td)?;
-            pool.create_snap(thin_id + 1, thin_id)?;
+            pool.close_thin(fix, td)?;
+            pool.create_snap(fix, thin_id + 1, thin_id)?;
             thin_id += 1;
             if thin_id >= 10 {
-                pool.delete_thin(thin_id - 10)?;
+                pool.delete_thin(fix, thin_id - 10)?;
             }
-            pool.commit()?;
-            td = pool.open_thin(thin_id)?;
+            pool.commit(fix)?;
+            td = pool.open_thin(fix, thin_id)?;
 
-            pool.stats_start();
+            pool.stats_start(fix);
             insert_count = 0;
         }
     }
 
-    pool.close_thin(td)?;
+    pool.close_thin(fix, td)?;
 
     Ok(())
 }
@@ -297,33 +295,41 @@ fn do_provision_rolling_snap(fix: &mut Fixture, thin_blocks: &[u64]) -> Result<(
 
     let mut pool = ThinPool::new(fix, 10240, 64, thin_blocks.len() as u64)?;
     let mut thin_id = 0;
-    pool.create_thin(0)?;
-    let td = pool.open_thin(0)?;
+    pool.create_thin(fix, 0)?;
+    let td = pool.open_thin(fix, 0)?;
 
+    let mut alloc_tracker = CostTracker::new("alloc-block.csv")?;
+    let mut insert_tracker = CostTracker::new("insert-block.csv")?;
     let mut insert_count = 0;
-    pool.stats_start();
+    pool.stats_start(fix);
     for thin_b in thin_blocks {
-        let data_b = pool.alloc_data_block()?;
-        pool.insert_block(&td, *thin_b, data_b)?;
+        alloc_tracker.begin(fix);
+        let data_b = pool.alloc_data_block(fix)?;
+        alloc_tracker.end(fix)?;
+
+        insert_tracker.begin(fix);
+        pool.insert_block(fix, &td, *thin_b, data_b)?;
+        insert_tracker.end(fix)?;
+
         insert_count += 1;
 
         if insert_count == commit_interval {
-            pool.commit()?;
-            pool.stats_report("provision", commit_interval)?;
+            pool.commit(fix)?;
+            pool.stats_report(fix, "provision", commit_interval)?;
 
-            pool.create_snap(thin_id + 1, 0)?;
+            pool.create_snap(fix, thin_id + 1, 0)?;
             thin_id += 1;
             if thin_id > 10 {
-                pool.delete_thin(thin_id - 10)?;
+                pool.delete_thin(fix, thin_id - 10)?;
             }
-            pool.commit()?;
+            pool.commit(fix)?;
 
-            pool.stats_start();
+            pool.stats_start(fix);
             insert_count = 0;
         }
     }
 
-    pool.close_thin(td)?;
+    pool.close_thin(fix, td)?;
 
     Ok(())
 }
@@ -331,7 +337,7 @@ fn do_provision_rolling_snap(fix: &mut Fixture, thin_blocks: &[u64]) -> Result<(
 // This test blows up, with massive amounts of instrs, read/write locks per insert.
 // Probably the fault of the space maps.
 fn test_provision_rolling_snaps(fix: &mut Fixture) -> Result<()> {
-    let thin_blocks = generate_runs(20_000, 20);
+    let thin_blocks = generate_runs(4_000, 20);
     do_provision_rolling_snap(fix, &thin_blocks)
 }
 
