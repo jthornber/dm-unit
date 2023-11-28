816f74551d790ed04d959363c0114bcedb82faaa
Joe Thornber <ejt@redhat.com>
2023-11-28 11:49:33 +0000

    [fixture] call_at() was freeing the ebreak instr incorrectly.
    It was using unmap() rather than free(), so it didn't get freed
    from the heap (just unmapped).

--- src/fixture.rs
+++ src/fixture.rs
@@ -261,7 +261,7 @@ impl Fixture {
         }
 
         let result = self.run_vm();
-        self.vm.mem.unmap(exit_addr)?;
+        self.vm.mem.free(exit_addr)?;
         match result {
             Ok(_) => {
                 // Not sure how we can get here
